6ada8ed419bd4a423562b15930216e4ee7c3dbd7
Ольга1213 <O_Mischenkova@mail.ru>
2024-04-22 16:09:42 +0500

    homework22

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var Dog = 8.0;
+        var Cat = 3.6;
+        var paper = 763789;
+        System.out.println("Dog8.0");
+        System.out.println("cat3.6");
+        System.out.println("paper763789");
+
+        var x = 8.0;
+        var result = 8.0 + 4;
+        System.out.println("dog8.0=" + result);//=120
+        var y = 3.6;
+        var Result = 3.6 + 4;
+        System.out.println("cat3.6=" + result);//3.6+4=7.6
+        var Paper = 763789;
+        System.out.println("paper763789=" + result);//763789+4=763793
+
+        var dog = 8.0;
+        System.out.println("dog8.0=" + result);//dog-3.5=4.5;
+        var cat = 3.6;
+
+        System.out.println("cat3.6=" + result);//cat-1.6=2.0;
+
+        System.out.println("paper763789=" + result);//763789-7639=756150;
+
+
+        var friend = 19;
+        System.out.println("friend19=" + result);//19+2=21;
+        System.out.println("friend21 =" + result);//21/7=3;
+
+        var frog = 3.5;
+        System.out.println("frog3.5 =" + result);//3.5*10=35;
+
+
+        var X = 78.2;
+        var Y = 82.7;
+        System.out.println("x+y=" + result);//78.2+82.7=160.9;
+        System.out.println("x-y=" + result);//x-y=4.5;
+
+        System.out.println("y%x=" + result);//78.2+82.7=160.9;
+        System.out.println("x-y=" + result);//y%x=1.057544;
+
+        var time = 640;
+        var workTime = 8;
+        var staff = x;
+
+        System.out.println("640%8=" + result);//time/workTime=80;var staff=174"
+
     }
 }
