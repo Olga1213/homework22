f34f7358260cdf0435c99026a225ea7c2b05a8cd
Ольга1213 <O_Mischenkova@mail.ru>
2024-04-28 21:50:40 +0500

    homework22

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,25 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-        System.out.println("Dog");
-        System.out.println("cat");
-        System.out.println("paper");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
         //2
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
-        System.out.println("Dog");
-        System.out.println("cat");
-        System.out.println("paper");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
         //3
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
-        System.out.println("Dog");
-        System.out.println("cat");
-        System.out.println("paper");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
         //4
         var friend = 19;
@@ -45,8 +45,8 @@ public class Main {
         //6
         var boxer1 = 78.2;
         var boxer2 = 82.7;
-        System.out.println("boxer1+boxer2");
-        System.out.println("boxer2-boxer1");
+        System.out.println(boxer1 + boxer2);
+        System.out.println(boxer2 - boxer1);
 
 
         //7
